2eaea6366293ee35178efbafa0a5390db7d5877b
Bullcka <166361451+Bullcka@users.noreply.github.com>
2024-06-01 15:31:10 +0500

    Update ProductReturnServiceTest.java

--- Autotesting/src/test/java/ru/shop/service/ProductReturnServiceTest.java
+++ Autotesting/src/test/java/ru/shop/service/ProductReturnServiceTest.java
@@ -1,12 +1,10 @@
 package ru.shop.service;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
 import ru.shop.exception.EntityNotFoundException;
 import ru.shop.model.*;
-import ru.shop.repository.OrderRepository;
 import ru.shop.repository.ProductReturnRepository;
 
 import java.time.LocalDate;
@@ -15,39 +13,37 @@ import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class ProductReturnServiceTest {
-    OrderService orderService = Mockito.mock(OrderService.class);
     private final ProductReturnRepository repository = Mockito.mock();
-    private final ProductReturnService productReturnService = new ProductReturnService(repository, orderService);
+    private final ProductReturnService productReturnService = new ProductReturnService(repository);
 
     @Test
-    void shouldAddProductReturn() {
+    public void shouldAddProductReturn() {
         //given
-        Order order = new Order(UUID.randomUUID(), UUID.randomUUID(), UUID.randomUUID(), 10, 10);
+        var order = new Order(UUID.randomUUID(), UUID.randomUUID(), UUID.randomUUID(), 3, 6);
+        var productReturn = new ProductReturn(UUID.randomUUID(), order.getId(), LocalDate.now(), order.getCount() - 1);
 
         //when
-        productReturnService.add(order, 10);
+        productReturnService.add(order, productReturn.getQuantity());
+        ArgumentCaptor<ProductReturn> orderArgumentCaptor = ArgumentCaptor.captor();
+
+        verify(repository, times(1)).save(orderArgumentCaptor.capture());
 
         //then
-        ArgumentCaptor<ProductReturn> argumentCaptor = ArgumentCaptor.captor();
-        ProductReturn savedProductReturn = argumentCaptor.getValue();
+        ProductReturn savedReturn = orderArgumentCaptor.getValue();
 
-        verify(repository).save(argumentCaptor.capture());
-        assertThat(savedProductReturn)
-                .returns(10, ProductReturn::getQuantity)
-                .returns(order.getId(), ProductReturn::getOrderId);
+        assertEquals(productReturn.getQuantity(), savedReturn.getQuantity());
+        assertEquals(order.getId(), savedReturn.getOrderId());
     }
 
     @Test
     public void shouldFindAllProductReturn() {
         //given
-        List<ProductReturn> productReturns = List.of(new ProductReturn(UUID.randomUUID(), UUID.randomUUID(), LocalDate.now(), 10));
+        List<ProductReturn> productReturns = List.of(new ProductReturn(UUID.randomUUID(), UUID.randomUUID(), LocalDate.now(), (long) 10));
 
         when(repository.findAll()).thenReturn(productReturns);
 
@@ -62,7 +58,7 @@ public class ProductReturnServiceTest {
     void shouldGetProductReturn() {
         //given
         UUID id = UUID.randomUUID();
-        ProductReturn productReturn = new ProductReturn(UUID.randomUUID(), UUID.randomUUID(), LocalDate.now(), 10);
+        ProductReturn productReturn = new ProductReturn(UUID.randomUUID(), UUID.randomUUID(), LocalDate.now(), (long) 10);
 
         when(repository.findById(id)).thenReturn(Optional.of(productReturn));
 
@@ -85,9 +81,9 @@ public class ProductReturnServiceTest {
     }
 
     @Test
-    public void shouldThrowWhenProductReturnNotFound() {
-        //then
+    public void shouldThrowWhenProductNotFound() {
+        // then
         assertThatThrownBy(() -> productReturnService.findById(UUID.randomUUID()))
                 .isInstanceOf(EntityNotFoundException.class);
     }
-} 
+}
